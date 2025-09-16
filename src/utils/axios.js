d38fe06d9c9ec4c3c1ba4db2701f3054f8da5d3a
raunaksomani25 <156603817+raunaksomani25@users.noreply.github.com>
2025-09-16 09:22:55 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,  
   headers: {
-    "x-api-key": process.env.VITE_API_SECRET_KEY,
+    "x-api-key": import.meta.env.VITE_API_SECRET_KEY,
   }
 });
 
